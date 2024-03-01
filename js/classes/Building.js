ab6b0fbe30976b76f83ff68be1d5c15168f49a6a
v-iinh <officiallyvt@gmail.com>
2024-02-29 20:01:48 -0500

    fixed fonts + added leaderboard

--- js/classes/Building.js
+++ js/classes/Building.js
@@ -1,61 +1,66 @@
 class Building extends Sprite {
-  constructor({ position = { x: 0, y: 0 } }) {
-    super({
-      position,
-      imageSrc: './img/calculator.png',
-      frames: {
-        max: 19
-      },
-      offset: {
-        x: 0,
-        y: 0
-      }
-    })
+	constructor({
+		position = {
+			x: 0,
+			y: 0
+		}
+	}) {
+		super({
+			position,
+			imageSrc: './img/calculator.png',
+			frames: {
+				max: 19
+			},
+			offset: {
+				x: 0,
+				y: 0
+			}
+		})
 
-    this.width = 40
-    this.height = 20
-    this.center = {
-      x: this.position.x + this.width / 2,
-      y: this.position.y + this.height / 2
-    }
-    this.projectiles = []
-    this.radius = 250
-    this.target
-  }
+		this.width = 40
+		this.height = 20
+		this.center = {
+			x: this.position.x + this.width / 2,
+			y: this.position.y + this.height / 2
+		}
+		this.projectiles = []
+		this.radius = 250
+		this.target
+	}
 
-  draw() {
-    super.draw()
-  }
+	draw() {
+		super.draw()
+	}
 
-  hover() {
-    c.beginPath()
-    c.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
-    c.fillStyle = 'rgba(0, 0, 255, 0.2)'
-    c.fill()
-  }
+	hover() {
+		c.beginPath()
+		c.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2)
+		c.fillStyle = 'rgba(0, 0, 255, 0.2)'
+		c.fill()
+	}
 
-  update() {
-    this.draw()
-    if (this.target || (!this.target && this.frames.current !== 0))
-      super.update()
+	update() {
+		this.draw()
+		if (this.target || (!this.target && this.frames.current !== 0))
+			super.update()
 
-    if (
-      this.target &&
-      this.frames.current === 0 &&
-      this.frames.elapsed % this.frames.hold === 0
-    )
-      this.shoot()
-  }
+		if (
+			this.target &&
+			this.frames.current === 0 &&
+			this.frames.elapsed % this.frames.hold === 0
+		)
+			this.shoot()
+	}
 
-  shoot() {
-    this.projectiles.push(
-      new Projectile({
-        position: {
-          x: this.center.x - 0,
-          y: this.center.y - 0
-        },
-        enemy: this.target
-      })
-    )
-  }
+	shoot() {
+		this.projectiles.push(
+			new Projectile({
+				position: {
+					x: this.center.x - 0,
+					y: this.center.y - 0
+				},
+				enemy: this.target
+			})
+		)
+	}
 }
--- lore.js
+++ lore.js
@@ -2,3 +2,9 @@ function lore() {
     document.getElementById('loreText').style.display = "flex";  
     document.getElementById('title').style.display = "none";  
 }
+
+function leaderboard(){
+    document.getElementById('loreText').style.display = "none";  
+    document.getElementById('title').style.display = "none";    
+    document.getElementById('hallOfFame').style.display = "flex";   
+}
